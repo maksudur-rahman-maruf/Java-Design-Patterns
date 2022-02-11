557cb59b9389f14ac04dec030cd74c892e3086c4
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2022-02-11 20:51:52 +0600

    Create Guitar.java

---
+++ SOLID Principles/Open Closed/Guitar.java
@@ -0,0 +1,17 @@
+public class Guitar {
+
+    private String model;
+    private String color;
+    private int price;
+
+    // Constructors, getters & setters
+}
+
+class ElectricGuitar extends Guitar {
+    private String electricPort;
+
+    // constructor, getters + setters
+
+}
+
+// By extending the Guitar class, we can be sure that our existing application won't be affected.
