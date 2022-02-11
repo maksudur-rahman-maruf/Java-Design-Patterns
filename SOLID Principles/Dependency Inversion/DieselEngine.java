68292b93c6a88e029227577c42bd9d68b69fe115
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2022-02-11 20:47:18 +0600

    Add files via upload

---
+++ SOLID Principles/Dependency Inversion/DieselEngine.java
@@ -0,0 +1,5 @@
+public class DieselEngine implements EngineInterface {
+    public void start() {
+        System.out.println("Car is consuming Diesel Engine!");
+    }
+}
---
+++ SOLID Principles/Dependency Inversion/PetrolEngine.java
@@ -0,0 +1,5 @@
+public class PetrolEngine implements EngineInterface {
+    public void start() {
+        System.out.println("Car is consuming Petrol Engine!");
+    }
+}
