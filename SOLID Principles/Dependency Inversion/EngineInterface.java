b196ae949985387909286d26de8d79c7e1d7648b
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2022-04-10 15:13:51 +0600

    Update EngineInterface.java

--- SOLID Principles/Dependency Inversion/EngineInterface.java
+++ SOLID Principles/Dependency Inversion/EngineInterface.java
@@ -1,3 +1,7 @@
 public interface EngineInterface {
     public void start();
 }
+
+## The Dependency Inversion Principle (DIP) states that:
+high level modules should not depend on low level modules.
+both should depend on abstractions.
